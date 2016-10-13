26649aebbf806b3fb1ace19cd02a3054d465f4b3
Lucas Currah <lucas.currah@gmail.com>
2016-10-13 13:35:38 -0400

    Working assignment

---
+++ KineticEnergy/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KineticEnergy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel vm = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void btnConvert(object sender, RoutedEventArgs e)
+        {
+            vm.calcKineticEnergy();
+        }
+
+        private void validateTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ KineticEnergy/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KineticEnergy
+{
+    class ViewModel : ObservableObject
+    {
+        private string _mass;
+        public string mass
+        {
+            get { return _mass; }
+            set { _mass = value; OnPropertyChanged(); }
+        }
+        private string _velocity;
+        public string velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; OnPropertyChanged(); }
+        }
+        private string _kineticEnergy;
+        public string kineticEnergy
+        {
+            get { return _kineticEnergy; }
+            set { _kineticEnergy = value; OnPropertyChanged(); }
+        }
+
+        public void calcKineticEnergy()
+        {
+            double kineticEnergyF;
+            double massF = double.Parse(_mass);
+            double velocityF = double.Parse(_velocity);
+            kineticEnergyF = Math.Pow(velocityF, 2) * massF * 0.5;
+            kineticEnergy = kineticEnergyF.ToString() + " (J)";
+        }
+    }
+}
